966352e96c325f9f41e97d21cf23ed1d725d3b9d
Loginov10 <artjomloginov111@gmail.com>
2024-10-28 13:19:37 +0000

    Изменено сообщение приветствия в index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
+  res.send('Hello, dear user!'); // Изменено на более дружелюбное приветствие
 });
 
 server.listen(port, hostname, () => {
